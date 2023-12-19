636dd61f337e35dbf6d0bcf0b1e6b5e28a63e5de
flaviodrummond <flaviosax20@gmail.com>
2023-12-19 12:09:56 -0300

    Primeiro projeto React, instalação da biblioteca styled-components, estilizando pelo react

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+background-color: black;
+`;
+
+export const H1 = styled.h1 `
+color: goldenrod;
+padding: 20px;
+margin-left: 50px;
+`
+
+export const Span = styled.span `
+color: orangered;
+`
