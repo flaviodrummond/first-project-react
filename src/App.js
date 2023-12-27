f448badd2abe1df40cf538c4c738f7dc615c6bd3
flaviodrummond <flaviosax20@gmail.com>
2023-12-27 17:48:47 -0300

    Aprendendo react hook - useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,  { useState } from "react";
+import React,  { useState, useRef } from "react";
 import { Container, H1, Image, ContainerContent, InputLabel, Input, Button, User } from "./styles";
 import People from './assets/people.svg';
 import Arrow from './assets/arrow.svg'
@@ -6,22 +6,29 @@ import Trash from './assets/trash.svg'
 
 const App = () => {
   const [ users, setusers] = useState([]);
-  const [ name, setName] = useState();
-  const [ age, setAge] = useState(); 
+  // const [ name, setName] = useState();
+  // const [ age, setAge] = useState(); 
+  const inputName = useRef();
+  const inputAge = useRef();
 
   function addNewUser (){
-    setusers([ ...users, { id: Math.random(), name, age }])
+    // setusers([ ...users, { id: Math.random(), name, age }])
+     
+    setusers([...users, { id: Math.random(),
+      name: inputName.current.value, 
+      age: inputAge.current.value}])
+  
   }
 
-  function changeName (event){
-    setName(event.target.value)
+  // function changeName (event){
+  //   setName(event.target.value)
 
-  }
+  // }
 
-  function changeAge (event) {
-    setAge(event.target.value)
+  // function changeAge (event) {
+  //   setAge(event.target.value)
 
-  }
+  // }
 
   return (<Container>
 
@@ -32,10 +39,11 @@ const App = () => {
       <H1> Olá !</H1>
 
       <InputLabel >Nome</InputLabel>
-      <Input onChange={changeName} placeholder="Nome"></Input>
+      <Input /*onChange={changeName} */ 
+      ref={inputName} placeholder="Nome"></Input>
 
       <InputLabel >Idade</InputLabel>
-      <Input type="number" onChange={changeAge} placeholder="Idade"></Input>
+      <Input type="number" /* onChange={changeAge} */    ref={inputAge} placeholder="Idade"></Input>
 
       <Button onClick={addNewUser} >Cadastrar <img alt="seta" src={Arrow}></img>
       </Button>
